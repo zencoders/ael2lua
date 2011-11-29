a5b99a57f325f4c346030a3db8dfb73df315690b
Sebastiano Merlino <etr@zencoders.org>
2011-11-29 01:38:25 +0100

    Added some bunch of documentation

--- handlers.h
+++ handlers.h
@@ -53,17 +53,61 @@ std::string time2iftime(TimeStruct* ts);
 **/
 void handleTimes(char* t1, char* t2, char* t3, char* day, char* md, char* m, TimeStruct* ts);
 
+/**
+ * Method used to fill a TimeStruct object.
+ * @param time string containing the time part of the representation
+ * @param day string containinng the day or the day range
+ * @param md string containing the day of month or the day of month range
+ * @param m string containing the month or the month range
+ * @param ts the resulting TimeStruct
+**/
 void handleTimes(char* time, char* day, char* md, char* m, TimeStruct* ts);
 
+/**
+ * Method used to build a complete if block
+ * @param head string containing the if head
+ * @param statement string containing the if body
+ * @return string containing the complete if block
+**/
 char* handleIf(char* head, char* statement);
 
+/**
+ * Method used to build a complete if block with an else statement
+ * @param head string containing the if head
+ * @param statement string containing the if body
+ * @param statement2 string containing the else body
+ * @return string containing the complete if block
+**/
 char* handleIfElse(char* head, char* statement, char* statement2);
 
+/**
+ * Method used to build the included name for a list of inclusions. This method works
+ * also to translate temporal inclusions.
+ * @param name The name of the file to include
+ * @param string containing the obtained new include name equivalent
+**/
 char* handleIncludedName(char* name);
 
+/**
+ * Method used to build a macro block
+ * @param name The macro name
+ * @param arglist The list of arguments passed to the macro
+ * @param stats Statements composing the macro body
+ * @return string representing the macro translated
+**/
 char* handleMacroDef(char* name,char* arglist, char* stats);
 
+/**
+ * Method used to build a context block
+ * @param name The name of the context block
+ * @param content The content of the context block
+ * @return string representing the context translated
+**/
 char* handleContext(char* name,char* content);
 
+/**
+ * Method used to annotate al encountered hints in code. This system is used to avoid a supplementary pass of the parser
+ * @param h The hint name
+**/
 void store_hint(char* h);
 #endif
